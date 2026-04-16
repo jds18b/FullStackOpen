632ae75e82bf6c33a2508ac5e0aa23ea3492fbcc
jds18b <jds18b@fsu.edu>
2026-04-16 19:33:40 -0400

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => <div>{person.name}</div>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleChangeName = (e) => {
+    //console.log(e.target.value);
+    setNewName(e.target.value)
+  }
+
+  const handleAddName = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <Person person={person} key={person.name} />)}
+    </div>
+  )
+}
+
+export default App
