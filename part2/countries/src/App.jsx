462166b82ad2faa75a4c376d7c2be281ba65cb57
jds18b <jds18b@fsu.edu>
2026-05-05 22:56:16 -0400

    2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -6,13 +6,13 @@ const SearchBar = ({ searchString, handleChangeSearch }) =>
   find countries: <input value={searchString} onChange={handleChangeSearch} />
 </div>
 
-const CountryDisplay = ({ countries, handleSetCountries }) => {
+const CountryDisplay = ({ countries, weather, handleSetCountries }) => {
   if(countries.length >= 10)
     return(
       <div>Too many matches, specify another filter</div>
     )
   if(countries.length === 1)
-    return(<CountryDetails country={countries[0]} />)
+    return(<CountryDetails country={countries[0]} weather={weather}/>)
   if(countries.length === 0)
     return(<div>No matches</div>)
   return(
@@ -20,13 +20,22 @@ const CountryDisplay = ({ countries, handleSetCountries }) => {
   )
 }
 
-const CountryDetails = ({ country }) => 
+const CountryDetails = ({ country, weather }) => 
 <>
   <h1>{country.name.common}</h1>
   <div>Capital {country.capital}</div>
   <div>Area {country.area}</div>
   <LanguageList languages={Object.values(country.languages)} />
   <img src={country.flags.png} alt={country.flags.alt} />
+  <WeatherDisplay country={country} weather={weather} />
+</>
+
+const WeatherDisplay = ({ country, weather }) => 
+<>
+  <h1>Weather in {country.capital}</h1>
+  <div>Temperature {weather.temperature} Celcius</div>
+  <img src={`https://openweathermap.org/payload/api/media/file/${weather.iconCode}.png`} alt="Could not source weather icon" />
+  <div>Wind {weather.wind} m/s</div>
 </>
 
 const LanguageList = ({ languages }) => 
@@ -44,6 +53,11 @@ const App = () => {
   const [ allCountries, setAllCountries ] = useState([])
   const [ searchString, setSearchString ] = useState('')
   const [ countries, setCountries] = useState([])
+  const [ weather, setWeather ] = useState({
+    temperature: "Default temperature",
+    wind: "Default wind speed",
+    iconCode: "01d"
+  })
 
   const handleChangeSearch = (e) => {
     setSearchString(e.target.value)
@@ -51,10 +65,10 @@ const App = () => {
   }
 
   const handleSetCountries = (country) => 
-    {
-      setCountries([country])
-      setSearchString(country.name.common)
-    }
+  {
+    setCountries([country])
+    setSearchString(country.name.common)
+  }
 
   // We can get a list of all the countries from a single API call and do any filtering we need locally
   // This cuts down on unnessecary API calls since it's unlikely that the list of countries in the world
@@ -63,14 +77,30 @@ const App = () => {
     axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
     .then((response) => setAllCountries(response.data))
   }, [])
-   
-  console.log(countries)
-  
+
+  useEffect(() => {
+    if(countries.length === 1)
+    {
+      const country = countries[0]
+      const lat = country.capitalInfo.latlng[0]
+      const lon = country.capitalInfo.latlng[1]
+      
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_API_KEY}`)
+      .then(response => {
+        const currentWeather = response.data
+        setWeather({
+          temperature: currentWeather.main.temp,
+          wind: currentWeather.wind.speed,
+          iconCode: currentWeather.weather[0].icon
+        })
+      }) 
+    }
+  }, [countries])
 
   return (
     <div>
       <SearchBar searchString={searchString} handleChangeSearch={handleChangeSearch} />
-      <CountryDisplay countries={countries} handleSetCountries={handleSetCountries} />
+      <CountryDisplay countries={countries} weather={weather} handleSetCountries={handleSetCountries} />
     </div>
   )
 }
