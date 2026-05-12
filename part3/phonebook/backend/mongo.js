e0f1778d9ba0ae664da6bc66caeee456c4d94858
jds18b <jds18b@fsu.edu>
2026-05-12 19:42:17 -0400

    3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+
+if(process.argv.length !== 3 && process.argv.length !== 5)
+{
+    console.log("Usage: node mongo.js password [name number]")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://jds18b_db:${password}@cluster0.zxuzooh.mongodb.net/phonebook?appName=Cluster0`
+
+mongoose.set("strictQuery", false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3)
+{
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+else
+{
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then( result => {
+        console.log(`Added ${person.name} ${person.number}`)
+        mongoose.connection.close()
+    })
+}
