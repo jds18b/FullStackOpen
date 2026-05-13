888d0f24f5b4ed3d2a353c2ec9883598ba99eec3
jds18b <jds18b@fsu.edu>
2026-05-12 20:51:00 -0400

    3.15 and 3.16

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -62,10 +62,15 @@ app.get("/api/persons", (req, res) => {
     })
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     Person.findById(req.params.id).then(person => {
-        res.json(person)
-    }) 
+        if(person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 app.post("/api/persons", (req, res) => {
@@ -88,11 +93,12 @@ app.post("/api/persons", (req, res) => {
     })
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+app.delete("/api/persons/:id", (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get("/info", (req, res) => {
@@ -106,6 +112,18 @@ const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: "Unknown endpoint" })
 }
 app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+
+    if(error.name === "CastError") {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
