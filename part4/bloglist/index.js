0310523abe3c1f9343f2f85144f7b41b438c5218
jds18b <jds18b@fsu.edu>
2026-05-18 20:53:02 -0400

    Added blogs route controller

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,7 +2,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
@@ -16,19 +16,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
